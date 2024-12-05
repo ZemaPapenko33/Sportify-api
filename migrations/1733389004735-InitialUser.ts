d765ff836897c89cd129f13a180e99690cb9b9c9
Zahar Papenko <zakharpapenko@gmail.com>
2024-12-05 14:38:45 +0300

    fix: migrations

--- migrations/1733389004735-InitialUser.ts
+++ migrations/1733389004735-InitialUser.ts
@@ -50,44 +50,6 @@ export class InitialUser1733389004735 implements MigrationInterface {
         ],
       }),
     );
-
-    await queryRunner.createTable(
-      new Table({
-        name: 'users_courses',
-        columns: [
-          {
-            name: 'user_id',
-            type: 'uuid',
-          },
-          {
-            name: 'course_id',
-            type: 'uuid',
-          },
-        ],
-      }),
-    );
-
-    await queryRunner.createForeignKey(
-      'users_courses',
-      new TableForeignKey({
-        columnNames: ['user_id'],
-        referencedColumnNames: ['id'],
-        referencedTableName: 'users',
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
-      }),
-    );
-
-    await queryRunner.createForeignKey(
-      'users_courses',
-      new TableForeignKey({
-        columnNames: ['course_id'],
-        referencedColumnNames: ['id'],
-        referencedTableName: 'courses',
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
-      }),
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- migrations/1733389027317-InitialCourse.ts
+++ migrations/1733389027317-InitialCourse.ts
@@ -102,6 +102,44 @@ export class InitialCourse1733389027317 implements MigrationInterface {
         onUpdate: 'CASCADE',
       }),
     );
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'users_courses',
+        columns: [
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'course_id',
+            type: 'uuid',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'users_courses',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'users_courses',
+      new TableForeignKey({
+        columnNames: ['course_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'courses',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
